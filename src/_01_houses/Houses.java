63e674c61d73ea45ae665ab1f36be3b1644a2db7
root <root@e009a1696115>
2020-08-14 18:28:04 +0000

    hi

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,38 +1,89 @@
 package _01_houses;
 
 import java.awt.Color;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
+
 public class Houses {
+	Robot house = new Robot ("mini");
+Random ran = new Random();
+int houseSize = 0;
 	public void run() {
 		
-	int height = newRandom().nextInt(150+50);
-		Robot house = new Robot ("mini");
+//	int height = new Random().nextInt(150 (+50));
 		house.penDown();
+		house.setWindowColor(0, 0, 0);
 		house.setSpeed(100);
 		house.moveTo(20, 500);
 		for (int i = 0; i < 10; i++) {
-	house.move(100);
-	house.turn(90);
-	house.move(25);
-	house.turn(90);
-	house.move(100);
-	house.turn(-90);
-	house.setPenColor(0, 225, 0);
-	house.move(25);
-	house.turn(-90);
-	house.setPenColor(Color.BLACK);
-	}
-	}
-
-	private int newRandom() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	private void nextInt(int i) {
-		// TODO Auto-generated method stub
 		
-	}
+			 houseSize = ran.nextInt(3);
+		if(houseSize == 0) {
+			drawHouse("small");
+			
+		}if(houseSize == 1) {
+		drawHouse("medium");
+	}if(houseSize == 2) {
+		drawHouse("large");
+
 }
+	/*
+	house.move(100);
+	house.turn(45);
+	house.move(20);
+	house.turn(90);
+	house.move(20);
+	house.turn(45);
+	house.move(100);
+	*/
+
+}
+	}
+		
+  //  “small”                 60
+
+  //  “medium”             120
+
+   // “large”                 250 
+public void drawHouse(int height) {
+	house.setRandomPenColor();
+house.move(height);
+if(houseSize == 2 ) {
+	drawPointyRoof();
+}else
+
+house.move(height);
+house.turn(-90);
+house.setPenColor(0, 225, 0);
+house.move(25);
+house.turn(-90);
+house.setPenColor(Color.BLACK);
+	
+
+}
+public void drawFlatRoof() {
+	house.turn(90);
+	house.move(25);
+	house.turn(90);
+}
+public void drawPointyRoof() {
+	house.turn(45);
+	house.move(25);
+	house.turn(90);
+	house.move(25);
+	house.turn(45);
+
+}
+	public void drawHouse(String size) {
+		if (size.equals ("small")) {
+drawHouse(60);
+		}else if (size.equals ("medium")) {
+			drawHouse(120);
+			}else if (size.equals ("large")) {
+				drawHouse(250);
+			}
+	}
+	}
+
