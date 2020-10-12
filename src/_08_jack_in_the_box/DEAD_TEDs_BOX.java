19ff5d3d03a6bff21c583dd7438598c8c98a62e3
root <root@9a17f8c21916>
2020-10-12 20:25:18 +0000

    NUMNUMZZZZZZZZ

--- src/_08_jack_in_the_box/DEAD_TEDs_BOX.java
+++ src/_08_jack_in_the_box/DEAD_TEDs_BOX.java
@@ -1,7 +1,73 @@
 package _08_jack_in_the_box;
 
-public class DEAD_TEDs_BOX {
-public static void main(String[] args) {
-	
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DEAD_TEDs_BOX implements ActionListener {
+int score = 0;
+	public void showPicture(String fileName) {
+	     try {
+	          
+	    	 JLabel imageLabel = createLabelImage("jackInTheBox.png");
+	          JFrame frame = new JFrame();
+	          
+	         
+
+
+              
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
+public void numnum() {
+	JFrame frame = new JFrame();
+	 JButton button = new JButton();
+     frame.add(button);
+     button.addActionListener(this);
+	   frame.setVisible(true);
+  frame.pack();
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  
 }
-}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if(score ==4) {
+			new DEAD_TEDs_BOX().showPicture("jackInTheBox.png");
+		}
+		score += 1;
+			
+		
+	}
+	}
+
+
+
---
+++ src/_08_jack_in_the_box/DEAD_TEDs_ON_SWITCH.java
@@ -0,0 +1,10 @@
+package _08_jack_in_the_box;
+
+public class DEAD_TEDs_ON_SWITCH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new DEAD_TEDs_BOX().numnum();
+	}
+
+}
